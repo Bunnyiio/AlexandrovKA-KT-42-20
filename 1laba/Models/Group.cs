fb527308d906d5cfc4a186994ed52471511b2f33
AlexandrovKA-KT-42-20 <zabraq@mail.ru>
2023-10-12 14:38:47 +0300

    add UnitTests

--- 1laba/Models/Group.cs
+++ 1laba/Models/Group.cs
@@ -1,9 +1,16 @@
-﻿namespace _1laba.Models
+﻿using System.Text.RegularExpressions;
+
+namespace _1laba.Models
 {
     public class Group
     {
         public int GroupId { get; set; }
 
         public string GroupName { get; set; }
+
+        public bool IsValidGroupName()
+        {
+            return Regex.Match(GroupName, @"/\D*-\d*-\d\d/g").Success;
+        }
     }
 }
---
+++ AlexandrovKA-KT-42-20.UnitTests/GroupTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _1laba.Models;
+
+namespace AlexandrovKA_KT_42_20.UnitTests
+{
+    public class GroupTests
+    {
+        [Fact]
+        public void IsValidGroupName_KT4220_True()
+        {
+            //arrange
+            var testGroup = new Group
+            {
+                GroupName = "KT-41-20"
+            };
+
+            //act
+            var result = testGroup.IsValidGroupName();
+
+            //assert
+            Assert.True(result);
+        }
+    }
+}
